1ea880504a6a13d894ed15d5a863cb941aba8c43
Vladyslav Romanov <vlad.vik.romanov@gmail.com>
2024-03-31 10:08:41 +0200

    add news update api call

--- main.py
+++ main.py
@@ -30,7 +30,8 @@ from fastapi import FastAPI
 
 from LLM_interactions.GPTRecommender import GPTRecommender
 from LLM_interactions.RecommendationTemplateConstructor import RecommendationTemplateConstructor
-
+from RSS_feed_collector.NewsRetriever import NewsRetriever
+from vector_database.NewsVectorStorage import NewsVectorStorage
 
 app = FastAPI()
 
@@ -38,6 +39,28 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/load_new_news")
+def load_new_news():
+    # List of RSS feed URLs
+    feeds_urls = [
+    'https://rss.nytimes.com/services/xml/rss/nyt/World.xml',
+    'https://rss.nytimes.com/services/xml/rss/nyt/US.xml',
+    'https://rss.nytimes.com/services/xml/rss/nyt/Business.xml',
+    'https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml',
+    'https://rss.nytimes.com/services/xml/rss/nyt/Science.xml',
+    'https://rss.nytimes.com/services/xml/rss/nyt/Health.xml'
+    ]
+
+    # Retrieve news from RSS feeds
+    news_retriever = NewsRetriever(feeds_urls)
+    news_df = news_retriever.retrieve_news()
+
+    # Store news title+summary as embeddings in a vector database with metadata
+    news_vector_storage = NewsVectorStorage()
+    news_vector_storage.load_news(news_dataframe=news_df)
+
+    return {"status": "success"}
 
 @app.get("/get_recommendations/{user_id}")
 def get_recommendations(user_id: str):
